8656c8c169946c1a5bf0960685d0f773d9893f2f
floj <floj@users.noreply.github.com>
2024-12-04 21:19:32 +0100

    day4: part 2

--- 04/go/main.go
+++ 04/go/main.go
@@ -10,62 +10,91 @@ const XMAS = "XMAS"
 const XMAS_REVERSE = "SAMX"
 
 func main() {
-	cnt, err := countXMAS("input.txt")
-	if err != nil {
+	if err := run(); err != nil {
 		fmt.Fprintf(os.Stderr, "puzzle errored with %v\n", err)
 		os.Exit(1)
 	}
-	fmt.Fprintf(os.Stdout, "occurences: %d\n", cnt)
 }
 
-func log(a ...any) {
-	if true {
-		fmt.Fprintln(os.Stderr, a...)
-	}
-
-}
-
-func countXMAS(inputFile string) (int, error) {
-	bytes, err := os.ReadFile(inputFile)
+func run() error {
+	bytes, err := os.ReadFile("input.txt")
 	if err != nil {
-		return -1, err
+		return err
 	}
+
+	// part 1
+	xmas, err := countXMAS(bytes[:])
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(os.Stdout, "occurences part 1: %d\n", xmas)
+
+	// part 2
+	masX, err := countMASX(bytes[:])
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(os.Stdout, "occurences part 2: %d\n", masX)
+	return nil
+}
+
+func countMASX(input []byte) (int, error) {
+	patterns := []rect{
+		toRect(mas1),
+		toRect(mas2),
+		toRect(mas3),
+		toRect(mas4),
+	}
+
+	m := NewMatrix(input)
 	occurences := 0
 
-	m := NewMatrix(bytes)
+	for y := range m.NumRows() {
+		for x := range m.NumCols() {
+			r, ok := m.Rect(x, y, 3, 3)
+			if !ok {
+				continue
+			}
+			for _, p := range patterns {
+				if p.Matches(r) {
+					occurences++
+					break
+				}
+			}
+		}
+	}
+
+	return occurences, nil
+
+}
+
+func countXMAS(input []byte) (int, error) {
+
+	m := NewMatrix(input)
+	occurences := 0
 
 	for y := range m.NumRows() {
 		for x := range m.NumCols() {
 			// horizontal
-			seq, ok := m.Seq(len(XMAS), x, y, 1, 0)
-			log("h", x, y, ok, seq)
-			if ok && isXMAS(seq) {
+			if seq, ok := m.Seq(len(XMAS), x, y, 1, 0); ok && isXMAS(seq) {
 				occurences++
 			}
 
 			// vertical
-			seq, ok = m.Seq(len(XMAS), x, y, 0, 1)
-			log("v", x, y, ok, seq)
-			if ok && isXMAS(seq) {
+			if seq, ok := m.Seq(len(XMAS), x, y, 0, 1); ok && isXMAS(seq) {
 				occurences++
 			}
 
 			// diagonal form top left to bottom right
-			seq, ok = m.Seq(len(XMAS), x, y, 1, 1)
-			log("d1", x, y, ok, seq)
-			if ok && isXMAS(seq) {
+			if seq, ok := m.Seq(len(XMAS), x, y, 1, 1); ok && isXMAS(seq) {
 				occurences++
 			}
 
 			// diagonal form top right to bottom left
-			log("d2", x, y, ok, seq)
-			seq, ok = m.Seq(len(XMAS), x, y, -1, 1)
-			if ok && isXMAS(seq) {
+			if seq, ok := m.Seq(len(XMAS), x, y, -1, 1); ok && isXMAS(seq) {
 				occurences++
 			}
-			log("----")
 		}
-		log("####")
 	}
 
 	return occurences, nil
@@ -76,7 +105,7 @@ func isXMAS(s string) bool {
 }
 
 func NewMatrix(s []byte) matrix {
-	data := bytes.Split(s, []byte{'\n'})
+	data := bytes.Split(s, []byte("\n"))
 	return matrix(data)
 }
 
@@ -105,6 +134,59 @@ func (m matrix) Get(x, y int) (byte, bool) {
 	return row[x], true
 }
 
+type rect [][]byte
+
+func (r rect) Matches(other [][]byte) bool {
+	if len(r) != len(other) {
+		return false
+	}
+	for i := range other {
+		or := other[i]
+		rr := r[i]
+		if len(rr) != len(or) {
+			return false
+		}
+		for j := range rr {
+			if rr[j] == '.' {
+				continue
+			}
+			if or[j] != rr[j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (r rect) String() string {
+	buf := bytes.Buffer{}
+	for i, row := range r {
+		if i > 0 {
+			buf.WriteByte('\n')
+		}
+		for _, b := range row {
+			buf.WriteByte(b)
+		}
+	}
+	return buf.String()
+}
+
+func (m *matrix) Rect(x, y, w, h int) (rect, bool) {
+	r := make([][]byte, 0, h)
+	for ih := range h {
+		row := []byte{}
+		for iw := range w {
+			v, ok := m.Get(x+iw, y+ih)
+			if !ok {
+				return nil, false
+			}
+			row = append(row, v)
+		}
+		r = append(r, row)
+	}
+	return rect(r), true
+}
+
 func (m *matrix) Seq(len, startX, startY, offsetX, offsetY int) (string, bool) {
 	buf := bytes.Buffer{}
 	x, y := startX, startY
@@ -119,4 +201,35 @@ func (m *matrix) Seq(len, startX, startY, offsetX, offsetY int) (string, bool) {
 
 	}
 	return buf.String(), true
+}
+
+const mas1 = `
+M.S
+.A.
+M.S
+`
+
+const mas2 = `
+S.M
+.A.
+S.M
+`
+
+const mas3 = `
+M.M
+.A.
+S.S
+`
+
+const mas4 = `
+S.S
+.A.
+M.M
+`
+
+func toRect(s string) rect {
+	b := []byte(s)
+	b = bytes.TrimSpace(b)
+	bb := bytes.Split(b, []byte{'\n'})
+	return rect(bb)
 }
